402ae753045bbc7768782e2d91bf4af18d61e7cf
weixinyi <65204427+rookiewxy@users.noreply.github.com>
2023-04-03 20:05:01 +0800

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@
  * @Author: wxy
  * @Description: 
  * @Date: 2023-03-13 18:22:02
- * @LastEditTime: 2023-03-13 19:26:04
+ * @LastEditTime: 2023-04-03 18:37:40
  */
 import Demo from './page/demo'
+import './App.css'
 
 function App() {
 
--- src/page/demo/index.tsx
+++ src/page/demo/index.tsx
@@ -1,12 +1,20 @@
 /*
  * @Author: wxy
  * @Description:
+ * @Date: 2023-03-31 12:05:56
+ * @LastEditTime: 2023-04-03 20:01:42
+ */
+/*
+ * @Author: wxy
+ * @Description:
  * @Date: 2023-03-13 19:14:40
- * @LastEditTime: 2023-03-15 19:05:07
+ * @LastEditTime: 2023-03-31 13:31:16
  */
 import React, { useEffect, useState } from "react";
 import * as THREE from "three";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import {
   CSS3DObject,
@@ -19,7 +27,6 @@ let camera: any = null;
 let scene: any = null;
 let renderer: any = null;
 let controls: any = null;
-const cameraPos = new THREE.Vector3(0, 0, 1500);
 
 const Demo = () => {
   const [data, setData] = useState("");
@@ -34,6 +41,9 @@ const Demo = () => {
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = false;
+    // renderer.setClearColor("#ffffff", 1);
+    renderer.shadowMap.enabled = true;
     container?.appendChild(renderer.domElement);
 
     labelRenderer = new CSS3DRenderer();
@@ -41,69 +51,100 @@ const Demo = () => {
     labelRenderer.domElement.style.position = "absolute";
     labelRenderer.domElement.style.top = "0px";
     container?.appendChild(labelRenderer.domElement);
-    labelRenderer.domElement.addEventListener("click", onMouseClick);
-
-  }
+    // labelRenderer.domElement.addEventListener("click", onMouseClick);
+  };
 
   // 初始化相机
   const initCamera = () => {
     camera = new THREE.PerspectiveCamera(
-      90,
+      45,
       window.innerWidth / window.innerHeight,
-      0.1,
-      10000
+      1,
+      100000
     );
-    camera.position.set(cameraPos.x, cameraPos.y, cameraPos.z);
-  }
+    camera.position.set(0, 400, 5000);
+    camera.lookAt(0, 1, 0);
+  };
 
   // 初始化场景
   const initScene = () => {
     scene = new THREE.Scene();
-  }
+  };
 
   // 初始化光源
   const initLight = () => {
     // 环境光
-    scene.add(new THREE.AmbientLight("#494b52", 4));
+    scene.add(new THREE.AmbientLight("#9d9ea1", 4));
+
     // 平行光
-    const light = new THREE.DirectionalLight(16777215, 1);
-    scene.add(light);
+    const dirLight = new THREE.DirectionalLight("red");
+    dirLight.position.set(180, 80, 100);
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.top = 2;
+    dirLight.shadow.camera.bottom = -2;
+    dirLight.shadow.camera.left = -2;
+    dirLight.shadow.camera.right = 2;
+    dirLight.shadow.camera.near = 0.1;
+    dirLight.shadow.camera.far = 40;
+    scene.add(dirLight);
+  };
 
-  }
-
-  const initOrbitControls = () =>{
+  const initOrbitControls = () => {
     controls = new OrbitControls(camera, labelRenderer.domElement);
     controls?.update();
-  }
+  };
 
   const initFbxModel = () => {
-    const loader = new FBXLoader();
-    loader.load(
-      "model/a.fbx",
-      (object) => {
-        object.traverse(function (child) {
-          if (child.isMesh) {
-            child.castShadow = true;
-            child.receiveShadow = true;
+    const arr = ["对象556"];
+    const gltfLoader = new GLTFLoader();
+
+    gltfLoader.load(
+      "./model/building.glb",
+      (gltf) => {
+        gltf.scene.traverse((child) => {
+          console.log(child.userData.name);
+          if (arr.indexOf(child.userData.name) != -1) {
+            const css3dObject = createTag(child);
+            console.log(css3dObject, child);
+            gltf.scene.add(css3dObject);
           }
         });
-        object.position.y = -300 
-        object.scale.multiplyScalar(0.1);
-        scene.add(object);
+        scene.add(gltf.scene);
       },
       onProgress,
       onError
     );
-  }
+    // const loader = new FBXLoader();
+    // loader.load(
+    //   "model/untitled.fbx",
+    //   (object) => {
+    //     object.traverse(function (child) {
+    //         console.log(child);
+    //       if (child.isMesh) {
+    //         child.castShadow = true;
+    //         child.receiveShadow = true;
+    //       }
+    //       if (arr.indexOf(child.name) != -1) {
+    //         const css3dObject = createTag(child);
+    //         scene.add(css3dObject);
+    //       }
+    //     });
+    //     object.scale.multiplyScalar(0.1);
+    //     scene.add(object);
+    //   },
+    //   onProgress,
+    //   onError
+    // );
+  };
 
   const initContainer = () => {
-    initRenderer()
-    initCamera()
-    initScene()
-    initLight()
-    initOrbitControls()
-    initFbxModel()
-    addCSS3DLabelToScene();
+    initRenderer();
+    initCamera();
+    initScene();
+    initLight();
+    initOrbitControls();
+    initFbxModel();
+    // addCSS3DLabelToScene();
     animate();
   };
 
@@ -126,6 +167,24 @@ const Demo = () => {
     css3DObject.visible = false;
   };
 
+  const createTag = (object3d) => {
+    // 创建各个区域的元素标签
+    const element = document.createElement("div");
+    element.className = "elementTag";
+    element.innerHTML = `
+      <div class="elementContent">
+        <h3>${object3d.name}</h3>
+        <p>温度：26℃</p>
+        <p>湿度：50%</p>
+      </div>
+    `;
+
+    const objectCSS3D = new CSS3DObject(element);
+    objectCSS3D.position.copy(object3d.position);
+    objectCSS3D.scale.set(0.2, 0.2, 0.2);
+    return objectCSS3D;
+  };
+
   const onMouseClick = (event) => {
     const raycaster = new THREE.Raycaster(); // 初始化光线追踪
     const vector = new THREE.Vector2(
@@ -140,8 +199,8 @@ const Demo = () => {
       css3DObject.position.y = intersects[0].point.y;
       console.log(intersects[0].object);
       intersects[0].object.material = new THREE.MeshPhongMaterial({
-        color:0xff0000
-      })
+        color: 0xff0000,
+      });
       setData(intersects[0].object.name);
     } else {
       css3DObject.visible = false;
